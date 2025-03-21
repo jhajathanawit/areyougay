3a2a213b055d9a63a98e75a4004330d98cab8ebc
jhajathanawit <jhajathanawit@gmail.com>
2025-03-22 00:20:37 +0700

    add p'lham jai gayray video

--- src/pages/DetailPage/Detailpage.tsx
+++ src/pages/DetailPage/Detailpage.tsx
@@ -1,14 +1,20 @@
-
-
 const Detailpage = () => {
-    return(
-        <div className="flex flex-col items-center mt-10 h-screen space-y-4">
-            <h1 className="text-bold text-3xl">
-                OK YOU GAY
-            </h1>
-        </div>
-
-    )
-
-}
+  return (
+    <div className="flex flex-col items-center pt-10 h-screen w-screen space-y-4 bg-pink-100">
+      <div className="flex items-center w-screen gap-4 p-2 rounded bg-pink-200 lg:w-1/2 lg:h-auto">
+        <iframe
+          width="1250"
+          height="703"
+          src="https://www.youtube.com/embed/kqNrhzTnD44?autoplay=1"
+          title="จอมขวัญ - โก๊ะ นิพนธ์ Slowed"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <h1 className="font-bold text-3xl">OK YOU GAY</h1>
+    </div>
+  );
+};
 export default Detailpage;
